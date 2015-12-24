823d1b1395302e51750817512be82d6d7d0465fe
teracamo <fromosia@gmail.com>
2015-12-25 03:29:59 +0800

    Add mesh grid

--- IodineSeed.py
+++ IodineSeed.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 
 import multiprocessing as mp
-from scipy import integrate
+from scipy import integrate, interpolate
 import numpy as np
 import multiprocessing as mp
+
+r_thetaTable = [[0.333,0.370,0.442,0.488,0.520,0.550],
+                [0.400,0.429,0.497,0.535,0.561,0.587],
+                [0.519,0.537,0.580,0.609,0.630,0.645],
+                [0.716,0.705,0.727,0.743,0.752,0.760],
+                [0.846,0.834,0.842,0.846,0.848,0.852],
+                [0.926,0.925,0.926,0.926,0.928,0.928],
+                [0.972,0.972,0.970,0.969,0.969,0.969],
+                [0.991,0.991,0.987,0.987,0.987,0.987],
+                [0.996,0.996,0.996,0.995,0.995,0.995],
+                [1.000,1.000,1.000,0.999,0.999,0.999]]
+
+theta = [0,5,10,20,30,40,50,60,70,80]
+r = [0.5,1,2,3,4,5]
+
+anistropy = [0.973,0.944,0.941,0.942,0.943,0.944]
 
 class IodineSeed(object):
     def __init__(self, center, length, orientation, activity):
@@ -12,6 +28,12 @@ class IodineSeed(object):
         self._length = length
         self._orientation = np.array(orientation)
         self._activity = activity
+        self._r_thetaTable = np.array(r_thetaTable)
+        self._anistropy = np.array(anistropy)
+        self._theta = np.array(theta)
+        self._r = np.array(r)
+        self._mg = self._constructInterpolationTable()
+
 
     def _sinLawGetAngle(self, m_r1, m_theta1, m_r2):
         m_theta2 = np.arcsin(m_r2*np.sin(m_theta1)/m_r1)
@@ -65,6 +87,15 @@ class IodineSeed(object):
         print m_intensity
         return m_intensity
 
+    def _constructInterpolationTable(self):
+        m_meshgrid = np.meshgrid(self._r,self._theta)
+        m_r = m_meshgrid[0].flatten()
+        m_theta = m_meshgrid[1].flatten()
+        m_table = self._r_thetaTable.flatten()
+        m_f = interpolate.interp2d(m_r,m_theta,m_table, kind="linear")
+        return m_f
+
+
     def SetSize(self, x, y, z):
         self._boundX = x
         self._boundY = y
@@ -93,7 +124,7 @@ class IodineSeed(object):
                     l_l_l_theta = np.rad2deg(np.arccos(np.dot(l_l_l_relativeUnitVect, self._orientation)))
                     l_l_l_R = np.abs(np.linalg.norm(l_l_l_relativeVect)*np.sin(np.deg2rad(l_l_l_theta)))
                     # result.append(pool.apply_async(self._energy(l_l_l_R, l_l_l_theta)[0]))
-                    m_scalarField[i,j,k] = self._energy(l_l_l_R, l_l_l_theta)[0]
+                    m_scalarField[i,j,k] = self._mg(l_l_l_R, l_l_l_theta)
                     print i,j,k
         # pool.close()
         # pool.join()
@@ -108,16 +139,16 @@ class IodineSeed(object):
         m_CPUnumber = mp.cpu_count()
         pool = mp.Pool(m_CPUnumber)
         result = []
-        m_scalarField = np.zeros([m_gridLengthX, m_gridLengthY], dtype=np.float32)
-        for i in xrange(m_gridLengthX):
-            for j in xrange(m_gridLengthY):
-                l_l_l_coord = np.array([-self._boundX/2. + self._spacing[0]*i, -self._boundY/2. + self._spacing[1]*j])
+        m_scalarField = np.zeros([m_gridLengthX/2, m_gridLengthY/2], dtype=np.float32)
+        for i in xrange(m_gridLengthX/2):
+            for j in xrange(m_gridLengthY/2):
+                l_l_l_coord = np.array([self._spacing[0]*i, self._spacing[1]*j])
                 l_l_l_relativeVect = self._center[0:2] - l_l_l_coord
                 l_l_l_relativeUnitVect = l_l_l_relativeVect/np.linalg.norm(l_l_l_relativeVect)
                 l_l_l_theta = np.rad2deg(np.arccos(np.dot(l_l_l_relativeUnitVect, self._orientation[0:2])))
                 l_l_l_R = np.abs(np.linalg.norm(l_l_l_relativeVect)*np.sin(np.deg2rad(l_l_l_theta)))
                 # result.append(pool.apply_async(self._energy(l_l_l_R, l_l_l_theta)[0]))
-                m_scalarField[i,j] = self._energy(l_l_l_R, l_l_l_theta)[0]
+                m_scalarField[i,j] = self._mg(l_l_l_R, l_l_l_theta)
                 print i,j
         # pool.close()
         # pool.join()
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 import IodineSeed
 
 def main():
-    seedexp = IodineSeed.IodineSeed([0,0,0], 4., [1,0,0], 1)
+    seedexp = IodineSeed.IodineSeed([0,0,0], 4., [0,-1,0], 1)
     # seedexp._energy(5, 100)
     # seedexp._energy(5, 80)
     seedexp.SetSize(10,10,10)
@@ -13,6 +13,7 @@ def main():
     out = seedexp.Update2D()
     print out
     np.save("out", out)
+    # print seedexp._mg(2,10)
 
 if __name__ == '__main__':
     main()
